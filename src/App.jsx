8b1069932b2aa4167f81b90f8f3181b05df47d7c
Kunal-Khare <kunalkhare47@gmail.com>
2025-03-12 23:56:47 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import { LoadingScreen } from "./components/LoadingScreen";
+import Navbar from "./components/Navbar";
 import "./index.css";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   return (
     <>
-      {!isLoaded && <LoadingScreen onLoad={() => setIsLoaded(true)} />} {" "}
+      {!isLoaded && <LoadingScreen onLoad={() => setIsLoaded(true)} />}{" "}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        }  bg-black text-gray-100`}
+      >
+        <Navbar/>
+      </div>
     </>
   );
 }
