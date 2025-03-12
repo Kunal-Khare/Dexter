9f3bf0a509ad1818ca1a6438760a5cc0d9b6aff1
Kunal-Khare <kunalkhare47@gmail.com>
2025-03-12 16:06:09 +0530

    animated-loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from "react";
+import { LoadingScreen } from "./components/LoadingScreen";
+import "./index.css";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
   return (
     <>
+      {!isLoaded && <LoadingScreen onLoad={() => setIsLoaded(true)} />} {" "}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
